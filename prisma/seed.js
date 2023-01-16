a5c2cba8d02a3bd787f8ac06f3382a8dbc661798
mlync87 <martylynch35@hotmail.com>
2023-01-16 18:17:24 +0000

    half way complete

--- prisma/seed.js
+++ prisma/seed.js
@@ -22,6 +22,32 @@ async function seed() {
       },
     },
   });
+
+  const createCustomerWithContact = await prisma.customer.create({
+    data: {
+      name: "Brett Hart",
+      contact: {
+        create: {
+          phone: "02890333333",
+          email: "thebesttherewas@gmail.com",
+        },
+      },
+    },
+  });
+
+  const createdMovie = await prisma.movie.create({
+    data: {
+      title: "Aliens",
+      runtimeMins: 120,
+    },
+  });
+
+  const createdScreening = await prisma.screening.create({
+    data: {
+      startsAt: 1830,
+    },
+  });
+
   // Don't edit any of the code below this line
   process.exit(0);
 }
